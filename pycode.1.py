4dce2fc15bd049c6f6e1d12c9902950841e86239
Rahul Vyas <rahulvyas@Rahuls-MacBook-Pro.local>
2018-09-03 23:24:13 +0530

    getAllEC2Instances- Now get all the instanceIDs

--- pycode.1.py
+++ pycode.1.py
@@ -1,12 +1,10 @@
 import boto3
-import click
+
 ec2 = boto3.client('ec2')
-
-
-
-
 response = ec2.describe_instances()
-
+#print(response)
+#print(type(response))
+#print(response['Reservations'][0]['Instances'][0]['InstanceId'])
 instanceId = response['Reservations'][0]['Instances'][0]['InstanceId']
 
 blockDeviceMappings = response['Reservations'][0]['Instances'][0]['BlockDeviceMappings']
@@ -22,19 +20,10 @@ def getAllVolumes():
             #print(block_device_mapping)
             volumes.append(block_device_mapping['Ebs']['VolumeId'])
 
-@click.command()
 def displayVolumes():
-    "Display All AWS Volumes"
     for v in volumes:
         print(v)
 
-def getAllEC2Instances():
-    for reservation in response['Reservations']:
-        for i in reservation['Instances']:
-            print(i['InstanceId'])
-
 #getVolumeOfInstance()
-#getAllVolumes()
-#displayVolumes()
-
-getAllEC2Instances()
+getAllVolumes()
+displayVolumes()
