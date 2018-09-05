4692a3446a2a14ebdbf24c2281f810517c1bbc2f
Rahul Vyas <rahulvyas@Rahuls-MacBook-Pro.local>
2018-09-05 20:04:13 +0530

    Code simplified

--- pycode.py
+++ pycode.py
@@ -1,40 +1,15 @@
 import boto3
 import click
-ec2 = boto3.client('ec2')
-
-
-
-
-response = ec2.describe_instances()
-
-instanceId = response['Reservations'][0]['Instances'][0]['InstanceId']
-
-blockDeviceMappings = response['Reservations'][0]['Instances'][0]['BlockDeviceMappings']
-#snapshot = ec2.create_snapshot(VolumeId='volume')
-volumes = []
-
-
-#print(response['Reservations'][1]['Instances'][0]['BlockDeviceMappings'])
-
-def getAllVolumes():
-    for reservation in response['Reservations']:
-        for block_device_mapping in reservation['Instances'][0]['BlockDeviceMappings']:
-            #print(block_device_mapping)
-            volumes.append(block_device_mapping['Ebs']['VolumeId'])
+ec2 = boto3.resource('ec2')
 
 @click.command()
-def displayVolumes():
-    "Display All AWS Volumes"
-    for v in volumes:
-        print(v)
+@click.option('--project', default=None,
+help="Only instances for project (tag Project:<name>)")
+def getAllEC2Instances(project):
+    "List EC2 Instances"
+    instances = []
+    for i in ec2.instances.all():    
+        print(i)
 
-def getAllEC2Instances():
-    for reservation in response['Reservations']:
-        for i in reservation['Instances']:
-            print(i['InstanceId'])
-
-#getVolumeOfInstance()
-#getAllVolumes()
-#displayVolumes()
 
 getAllEC2Instances()
